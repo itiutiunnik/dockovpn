6ba1776810178056f90273da1179287aa24490b5
Igor Tyutyunnik <itiutiunnik@gmail.com>
2023-07-22 14:17:11 +0200

    Small fix

--- api/variables.py
+++ api/variables.py
@@ -1,8 +1,8 @@
 import os
 
-CONFIG_DIR=os.environ('APP_PERSIST_DIR')
+CONFIG_DIR=os.environ['APP_PERSIST_DIR']
 CLIENT_FILE_NAME=f'{client_id}.ovpn'
 CLIENT_FILE_PATH=f'{CONFIG_DIR}/clients/{client_id}/{CLIENT_FILE_NAME}'
-SCRIPTS_DIR=os.environ('APP_INSTALL_PATH')
+SCRIPTS_DIR=os.environ['APP_INSTALL_PATH']
 GENSCRIPT=f"{SCRIPTS_DIR}/genclient.sh"
 RMSCRIPT=f"{SCRIPTS_DIR}/rmclient.sh"
